b16344878aadee81392a8f780c7fe6389983a981
Shruti Kapoor <shrkapoor@paypal.com>
2019-02-10 01:45:06 -0800

    GraphiQL and /graphql successfully working.

--- client/components/App.js
+++ client/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const App = props => (
-        <div>
-            <h1>Test works new</h1>
-            {props.children}
-        </div>
+    <div>
+        <h1>Testing things</h1>
+        {props.children}
+    </div>
 );
 
 export default App;
